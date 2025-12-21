0d7fc820383c9e4ae6bc34091ea3cf918f554826
Mohamed amine Tlili <foxweb585@gmail.com>
2025-12-21 20:15:09 +0100

    micrometer + load Balancer

---
+++ job-service/src/main/java/com/safalifter/jobservice/config/LoadBalancerConfig.java
@@ -0,0 +1,23 @@
+package com.safalifter.jobservice.config;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class LoadBalancerConfig {
+
+    @Bean
+    public ReactorLoadBalancer<ServiceInstance> randomLoadBalancer(
+            Environment environment,
+            LoadBalancerClientFactory factory) {
+
+        String serviceName = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RandomLoadBalancer(factory.getLazyProvider(serviceName, ServiceInstanceListSupplier.class), serviceName);
+    }
+}
